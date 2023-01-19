f8ec26e277f0b3b3c2559cb5e8a40f522b7d6f8d
Yogendra Raj <yogendraraj0206@gmail.com>
2023-01-19 09:49:25 +0530

    initial commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} {{ event.date }} </span>
+    <h4>{{ event.id }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+    ran: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
